c96181298f33bbc79e930d0cb2bd043d0b2ad319
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-02-13 23:25:36 +0300

    Создание mob'a и добавление логики его движения

---
+++ scenes/mob/Mob.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public partial class Mob : CharacterBody3D
+{
+	[Export]
+	public int MinSpeed { get; set; } = 10;
+	[Export]
+	public int MaxSpeed { get; set; } = 18;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		MoveAndSlide();
+	}
+
+	public void Initialize(Vector3 startPosition, Vector3 playerPosition) 
+	{
+		int randomSpeed = GD.RandRange(MinSpeed, MaxSpeed);
+
+		LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
+
+		RotateY((float)GD.RandRange(-Mathf.Pi / 4.0, Mathf.Pi / 4.0));
+		
+	}
+}
