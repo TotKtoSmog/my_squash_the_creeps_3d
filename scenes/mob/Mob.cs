e84fba1a5b6cd2b0adb33230f181ed1befb68033
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-02-14 00:28:33 +0300

    Добавление спавна mob'ов и исправление ошибок

--- scenes/mob/Mob.cs
+++ scenes/mob/Mob.cs
@@ -17,8 +17,8 @@ public partial class Mob : CharacterBody3D
 
 		RotateY((float)GD.RandRange(-Mathf.Pi / 4.0, Mathf.Pi / 4.0));
 	
-        Velocity = Vector3.Forward * randomSpeed;
-        Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
-    }
-    private void OnVisibilityNotifierScreenExited() => QueueFree();
+		Velocity = Vector3.Forward * randomSpeed;
+		Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+	}
+	private void OnVisibilityNotifierScreenExited() => QueueFree();
 }
