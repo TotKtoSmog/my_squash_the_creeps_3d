fa84b2a757d613cce0e5dc3f603490a26181a3e4
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-02-13 23:39:04 +0300

    Добавление удаления mob'а за пределами экрана и обновление его движения

--- scenes/mob/Mob.cs
+++ scenes/mob/Mob.cs
@@ -7,10 +7,7 @@ public partial class Mob : CharacterBody3D
 	[Export]
 	public int MaxSpeed { get; set; } = 18;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		MoveAndSlide();
-	}
+	public override void _PhysicsProcess(double delta) => MoveAndSlide();
 
 	public void Initialize(Vector3 startPosition, Vector3 playerPosition) 
 	{
@@ -19,6 +16,9 @@ public partial class Mob : CharacterBody3D
 		LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
 
 		RotateY((float)GD.RandRange(-Mathf.Pi / 4.0, Mathf.Pi / 4.0));
-		
-	}
+	
+        Velocity = Vector3.Forward * randomSpeed;
+        Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+    }
+    private void OnVisibilityNotifierScreenExited() => QueueFree();
 }
